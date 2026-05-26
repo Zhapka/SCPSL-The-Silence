485a885b21dc083c367ef3374b1fb5a1ffbbc954
catwithchezzburgermusic-lgtm <catwithchezzburgermusic@gmail.com>
2026-05-26 12:21:27 +0500

    Implemented New SkyBox

--- Assets/Scripts/Assembly-CSharp/SkyboxFollower.cs
+++ Assets/Scripts/Assembly-CSharp/SkyboxFollower.cs
@@ -2,26 +2,19 @@ using UnityEngine;
 
 public class SkyboxFollower : MonoBehaviour
 {
-    [Header("Ssilky")]
-    public Transform playerCamera;
-    public Material normalSkybox;
-    [Header("Colors")]
-    public Color darkColor = Color.black;
+    public Transform camera;
+
     public static bool iAm939;
 
     private void Update()
     {
-        if (playerCamera == null) return;
-        if (iAm939 || playerCamera.position.y < 800f)
+        if (iAm939 || camera.position.y < 800f)
         {
-            RenderSettings.skybox = null;
-            RenderSettings.ambientMode = UnityEngine.Rendering.AmbientMode.Flat;
-            RenderSettings.ambientLight = darkColor;
+            base.transform.position = Vector3.down * 12345f;
         }
         else
         {
-            RenderSettings.skybox = normalSkybox;
-            RenderSettings.ambientMode = UnityEngine.Rendering.AmbientMode.Skybox;
+            base.transform.position = camera.position;
         }
     }
 }
